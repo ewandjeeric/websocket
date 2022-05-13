97fa0ddd856784a16d6989061f65c9f668dbc114
ewandjeeric <ewandjeeric@gmail.com>
2022-05-13 09:27:47 +0100

    ..

---
+++ src/main/java/org/socket/ChatMessage.java
@@ -0,0 +1,20 @@
+package org.socket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+	private MessageType type;
+	private String content;
+	private String sender;
+
+	public enum MessageType {
+		CHAT, JOIN, LEAVE
+	}
+
+}
